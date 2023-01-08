0b422399d9e79e5aa7034b1641c1b4da77d6a504
ozkostay <ozkostay@mail.ru>
2023-01-08 15:47:57 +0500

    all done 080123

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Stars from './components/Stars';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Stars />
+      <Stars count={0} />
+      <Stars count={1} />
+      <Stars count={2} />
+      <Stars count={3} />
+      <Stars count={4} />
+      <Stars count={5} />
+      <Stars count={6} />
+      <Stars count={'star'} />
     </div>
   );
 }
